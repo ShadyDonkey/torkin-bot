37950ab9103e741bdc03c0e5debca9ed036e7bee
Inbestigator <inbestigatoryt@gmail.com>
2025-12-15 08:23:22 -0800

    refactor(middleware): simpler param spread

--- server/dressed.config.ts
+++ server/dressed.config.ts
@@ -1,23 +1,20 @@
-import type { MessageComponentInteraction, ModalSubmitInteraction } from 'dressed'
+import type { MessageComponentInteraction } from 'dressed'
 import type { ServerConfig } from 'dressed/server'
 
 export default {
   build: { root: 'bot' },
   middleware: {
-    components(...p: unknown[]) {
-      const i = p[0] as MessageComponentInteraction | ModalSubmitInteraction
-      return [
-        {
-          ...i,
-          async updateResponse(...p: Parameters<MessageComponentInteraction['update']>) {
-            if (i.history.some((h) => ['reply', 'deferReply', 'update', 'deferUpdate'].includes(h))) {
-              return i.editReply(...p)
-            }
-            return i.update(...p)
-          },
+    components: (i, ...p) => [
+      {
+        ...i,
+        updateResponse(...p: Parameters<MessageComponentInteraction['updateResponse']>) {
+          if (i.history.some((h) => ['reply', 'deferReply', 'update', 'deferUpdate'].includes(h))) {
+            return i.editReply(...p)
+          }
+          return i.update(...p)
         },
-        ...p.slice(1),
-      ]
-    },
+      },
+      ...p,
+    ],
   },
 } satisfies ServerConfig
