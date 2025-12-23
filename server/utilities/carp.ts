5198b79177cbc99bc6f35471a46428e22063f8ab
Inbestigator <inbestigatoryt@gmail.com>
2025-12-23 04:24:30 -0800

    refactor: loosen types in carp

--- server/utilities/carp.ts
+++ server/utilities/carp.ts
@@ -1,7 +1,7 @@
-import type { APIMessageComponent, APIModalComponent } from 'discord-api-types/v10'
+import type { APIBaseComponent, ComponentType } from 'discord-api-types/v10'
 import { TextDisplay } from 'dressed'
 
-type CarpNode = (APIMessageComponent | APIModalComponent | string | undefined | false | 0 | null) | CarpNode[]
+type CarpNode = (APIBaseComponent<ComponentType> | string | undefined | false | 0 | null) | CarpNode[]
 
 /**
  * **C**omponent **AR**ray **P**rocessor (a-la React)
@@ -9,9 +9,7 @@ type CarpNode = (APIMessageComponent | APIModalComponent | string | undefined | 
  * - Combines consecutive strings
  * - Turns strings into TextDisplays
  */
-export default function carp<T extends APIMessageComponent | APIModalComponent = APIMessageComponent>(
-  ...components: CarpNode[]
-): T[] {
+export default function carp<T extends APIBaseComponent<ComponentType>>(...components: CarpNode[]): T[] {
   const combined = (components.flat().filter(Boolean) as (T | string)[]).reduce<(T | string)[]>((acc, curr) => {
     const prev = acc.at(-1)
     if (typeof curr === 'string' && typeof prev === 'string') acc[acc.length - 1] = `${prev}\n${curr}`
