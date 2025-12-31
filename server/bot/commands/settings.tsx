951d76fb36276211350644d524d2372f3f3b1d82
Matt Hatcher <matthatcher@pm.me>
2025-12-31 17:31:07 -0500

    add initial settings file

---
+++ server/bot/commands/settings.tsx
@@ -0,0 +1,14 @@
+import { type CommandInteraction, Container } from '@dressed/react'
+import type { CommandConfig } from 'dressed'
+import { DEV_GUILD_ID, IS_IN_DEV } from '@/server/lib/config'
+
+export const config = {
+  description: 'Manage your settings for the bot',
+  default_member_permissions: IS_IN_DEV ? ['Administrator'] : undefined,
+  integration_type: IS_IN_DEV ? 'Guild' : 'User',
+  guilds: IS_IN_DEV ? [DEV_GUILD_ID] : undefined,
+} satisfies CommandConfig
+
+export default async function (interaction: CommandInteraction<typeof config>) {
+  return await interaction.reply(<Container>r</Container>, { ephemeral: true })
+}
