29ea4ae60c53d9a6941d6b9fca7309c428a4a18a
Matt Hatcher <matthatcher@pm.me>
2025-12-31 14:03:57 -0500

    feat: dedupe provider text for details

--- server/bot/utilities/commands/listings.tsx
+++ server/bot/utilities/commands/listings.tsx
@@ -2,11 +2,12 @@ import { Button, Container, Section, Separator, TextDisplay, Thumbnail } from '@
 import { type UndefinedInitialDataOptions, useQuery } from '@tanstack/react-query'
 import { format } from 'date-fns'
 import { h2 } from 'discord-fmt'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Fragment } from 'react/jsx-runtime'
 import { ItemActions, ListingPreview, PaginationButtons } from '@/server/bot/utilities/builders'
 import { getImageUrl, getItemWatchProviders } from '@/server/lib/tmdb'
 import type { StandardListing } from '@/server/lib/tmdb/types'
+import { DUPLICATE_PROVIDER_ID_MAPPING } from '@/server/lib/tmdb/watch-providers'
 import { paginateArray } from '@/server/utilities'
 import { TrendingMovieDetails, TrendingTvDetails, VoteSection } from '../tmdb'
 import ErrorPage from './error'
@@ -100,6 +101,13 @@ function Availability({ type, id }: Readonly<{ type: 'movie' | 'tv'; id: number 
     queryKey: ['availability', type, id],
     queryFn: () => getItemWatchProviders(type, { id, season: 1 }),
   })
+  const [providerText, setProviderText] = useState('...')
+
+  useEffect(() => {
+    if (query.data?.results?.US?.flatrate?.length) {
+      dedupeProviders(query.data.results.US.flatrate).then((text) => setProviderText(text))
+    }
+  }, [query.data?.results?.US?.flatrate])
 
   if (!query.data?.results) {
     return (
@@ -113,8 +121,22 @@ function Availability({ type, id }: Readonly<{ type: 'movie' | 'tv'; id: number 
   return (
     <TextDisplay>
       {query.data.results.US?.flatrate?.length && query.data.results.US.flatrate.length > 0
-        ? query.data.results.US?.flatrate?.map((p) => p.provider_name).join(', ')
+        ? providerText
         : 'Not Available'}
     </TextDisplay>
   )
+}
+
+async function dedupeProviders(
+  providers: {
+    logo_path?: string | undefined
+    provider_id: number
+    provider_name?: string | undefined
+    display_priority: number
+  }[],
+) {
+  const filtered = providers.filter((p) => !DUPLICATE_PROVIDER_ID_MAPPING[p.provider_id] && p.provider_name)
+  const sorted = filtered.sort((a, b) => a.display_priority - b.display_priority)
+
+  return sorted.map((p) => p.provider_name).join(', ')
 }
