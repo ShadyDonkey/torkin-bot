632537248973f734a199f1340473a98df5c3422b
Matt Hatcher <matthatcher@pm.me>
2025-12-31 14:03:21 -0500

    add Discord emoji cache helper

---
+++ server/lib/discord/helpers.ts
@@ -0,0 +1,16 @@
+import { listApplicationEmojis } from 'dressed'
+import { cache } from '@/server/lib/cache'
+
+export async function getApplicationEmojis() {
+  return cache.getOrSet(
+    'lib:discord:application-emojis',
+    async () => {
+      const response = await listApplicationEmojis()
+
+      return response.items ?? []
+    },
+    {
+      ttl: '1d',
+    },
+  )
+}
