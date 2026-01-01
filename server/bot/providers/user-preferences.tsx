1dc1ecdf8443377452c667d985290ba4cff9a9fb
Matt Hatcher <matthatcher@pm.me>
2026-01-01 18:31:55 -0500

    stop forgetting effects dumbass

--- server/bot/providers/user-preferences.tsx
+++ server/bot/providers/user-preferences.tsx
@@ -1,5 +1,5 @@
 import { inspect } from 'node:util'
-import { createContext, type PropsWithChildren, useContext, useState } from 'react'
+import { createContext, type PropsWithChildren, useContext, useEffect, useState } from 'react'
 import { db } from '@/server/lib/db'
 import type { UserPreference } from '@/server/zenstack/models'
 
@@ -27,20 +27,31 @@ export function useUserPreferences() {
 
 export function UserPreferencesProvider({ children, userId }: PropsWithChildren & { userId: string }) {
   const [dbPreferences, setDbPreferences] = useState<UserPreference | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
 
-  db.userPreference
-    .findUniqueOrThrow({
-      where: {
-        discordUserId: userId,
-      },
-    })
-    .then((preferences) => {
-      console.log('UserPreferencesProvider', inspect(preferences))
-      setDbPreferences(preferences)
-    })
-    .catch((error) => {
-      console.error('UserPreferencesProvider', error)
-    })
+  useEffect(() => {
+    db.userPreference
+      .findUniqueOrThrow({
+        where: {
+          discordUserId: userId,
+        },
+      })
+      .then((preferences) => {
+        console.log('UserPreferencesProvider', inspect(preferences))
+        setDbPreferences(preferences)
+        setLoading(false)
+      })
+      .catch((error) => {
+        setError('Error fetching user preferences')
+        setLoading(false)
+        console.error('Error fetching user preferences', inspect(error))
+      })
+  }, [userId])
+
+  if (loading) {
+    return 'Loading...'
+  }
 
   if (!dbPreferences) {
     return 'No user preferences found, please set them up in settings command first.'
