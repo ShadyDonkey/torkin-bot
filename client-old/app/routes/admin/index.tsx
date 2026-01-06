290a3bacb9b43c749fdf9aaf685e756aad3ddadb
Matt Hatcher <matthatcher@pm.me>
2026-01-06 15:39:43 -0500

    wtf...I deleted these

--- client-old/app/routes/admin/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { Route } from './+types'
-
-export function meta(_: Route.MetaArgs) {
-  return [{ title: 'Admin | Home' }]
-}
-
-export default function Index() {
-  return <div>Admin Index</div>
-}
