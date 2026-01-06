4a59d5dc7b26cdc03cf201a7a6eadfcd0f42eab4
Matt Hatcher <matthatcher@pm.me>
2026-01-05 20:26:40 -0500

    adding helpers and admin scaffolding

---
+++ server/routes/admin.ts
@@ -0,0 +1,44 @@
+import { handleRequest, installCommands } from 'dressed/server'
+import { Elysia, t } from 'elysia'
+import { StatusCodes } from 'http-status-codes'
+import { commands, components, config, events } from '../.dressed'
+import { auth, authMacro } from '../lib/auth'
+import { unwrap } from '../utilities'
+import { createErrorRes, createResponseSchema, createSuccessRes } from '../utilities/elysia/response'
+import { logger } from '../utilities/logger'
+
+export const admin = new Elysia({
+  name: 'admin',
+  prefix: 'api/admin',
+})
+  .use(authMacro)
+  .guard({
+    beforeHandle: async ({ status, request: { headers } }) => {
+      const session = await auth.api.getSession({ headers })
+      if (!session) {
+        return status(StatusCodes.UNAUTHORIZED)
+      }
+
+      if (session.user.role !== 'admin') {
+        return status(StatusCodes.FORBIDDEN)
+      }
+    },
+  })
+  .post(
+    '/bot/install-commands',
+    async () => {
+      const [err] = await unwrap(installCommands(commands))
+
+      if (err) {
+        logger.error(err)
+
+        return createErrorRes(err.message)
+      }
+
+      return createSuccessRes({ message: 'Commands installed successfully' })
+    },
+    {
+      schema: createResponseSchema(t.Object({ message: t.String() })),
+      auth: true,
+    },
+  )
