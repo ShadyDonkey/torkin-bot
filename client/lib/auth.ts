010e03d725fbbf3c4da3d71150e548ed67a7b312
Matt Hatcher <matthatcher@pm.me>
2026-01-06 14:53:09 -0500

    sgfdg

--- client/lib/auth.ts
+++ client/lib/auth.ts
@@ -2,6 +2,7 @@ import { adminClient } from 'better-auth/client/plugins'
 import { createAuthClient } from 'better-auth/react'
 
 export const authClient = createAuthClient({
-  baseUrl: process.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  basePath: '/auth',
   plugins: [adminClient()],
 })
