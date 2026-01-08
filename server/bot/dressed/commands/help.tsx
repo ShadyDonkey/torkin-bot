1c2f7b37e4f88a0ff7b3aaf8d37ab8a17306332a
Matt Hatcher <matthatcher@pm.me>
2026-01-07 20:02:46 -0500

    add donate button to help command too

--- server/bot/dressed/commands/help.tsx
+++ server/bot/dressed/commands/help.tsx
@@ -1,4 +1,5 @@
 import { ActionRow, Button, type CommandInteraction, TextDisplay } from '@dressed/react'
+import { link } from 'discord-fmt'
 import type { CommandConfig } from 'dressed'
 import { GENERIC_COMMAND_CONFIG } from '../../utilities'
 
@@ -13,6 +14,9 @@ export default async function (interaction: CommandInteraction<typeof config>) {
       <TextDisplay>
         If you're in need of help, want to report a bug, or have a suggestion, please reach out to us on our support
         server.
+        {'\n\n'}
+        Enjoying Torkin? Consider ${link('donating on Ko-Fi', '<https://ko-fi.com/matthatcher>')} to keep the project
+        alive and remain free.
       </TextDisplay>
       <ActionRow>
         <Button label="Support Server" url="https://discord.gg/RP36fV6MNe" />
